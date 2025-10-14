b1b59e21fe712870115ff0c6f44f2febbb5e2268
Kosuke Otaka <2311140001m@ed.fuk.kindai.ac.jp>
2025-10-14 10:05:44 +0000

    push to render.com

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,18 +1,29 @@
 class TopController < ApplicationController
-    def main
-        if session[:login_uid] !=nil
-           render "main"
-        else
-           render "login"
-        end
+  def main
+    if session[:login_uid].nil?
+      render "login"
+    else
+      render "main"
     end
+  end
 
-    def login
-        if params[:uid] == "kindai" and params[:pass] == "sanriko"
-           session[:login_uid] = params[:uid]
-           redirect_to top_main_path
-        else
-           render "error", status: 422
-        end
+  def login
+    uid = params[:uid]
+    pass = params[:pass]
+
+    user = User.find_by(uid: uid)
+
+    if user&.authenticate(pass)
+      session[:login_uid] = user.uid
+      redirect_to top_main_path
+    else
+    
+      render "error", status: 422
     end
+  end
+
+  def logout
+    session.delete(:login_uid)
+    redirect_to top_main_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   get 'top/main'
   post 'top/login'
+  get 'top/logout'
   root 'top#main'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
